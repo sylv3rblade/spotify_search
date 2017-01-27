5446323587bbd379d737c637b0e107e309b5189d
sylv3rblade <dev@sylv3rblade.com>
2017-01-27 10:13:23 +0800

    add spotify service for search

---
+++ app/services/spotify_services/search.rb
@@ -0,0 +1,28 @@
+module SpotifyServices
+  class Search
+    SEARCH_URL = 'https://api.spotify.com/v1/search'
+    def initialize(params)
+      @limit = params[:limit] || 50
+      @type = 'artist'
+      @query = params[:query]
+    end
+
+    def process
+      HTTParty.get("#{SEARCH_URL}/?#{query_string}")
+    end
+
+    private
+
+    def query_attrs
+      {
+        q: @query,
+        limit: @limit,
+        type: @type,
+      }
+    end
+
+    def query_string
+      query_attrs.to_query
+    end
+  end
+end
