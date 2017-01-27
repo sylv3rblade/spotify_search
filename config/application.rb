c19cc22454e981caa42cb7b4c2b3a9a58ed08460
sylv3rblade <dev@sylv3rblade.com>
2017-01-27 10:27:45 +0800

    api only

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module SpotifySearch
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.api_only = true
     config.autoload_paths += %W(#{Rails.root}/app/services)
   end
 end
